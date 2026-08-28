024e8179755502ee94b7d47f067e58a8d7741427
adityaverma054 <adityaverma054@gmail.com>
2026-08-29 02:21:18 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 set -e
-export PGPASSWORD="EWBXt5VbG57XDppAqGrrnSV8JVecaZOh"
+
+export PGPASSWORD="rj3lNB5fGAiu87WDUHAn99UhkF8yIKPI"
+
 exec python3 /usr/bin/odoo \
-    --db_host="dpg-da8ukrajnfac73c4mmsg-a.oregon-postgres.render.com" \
+    --db_host="dpg-da8v950n74is73e9i6tg-a" \
     --db_port="5432" \
-    --db_user="openeducat_db_user" \
-    --database="openeducat_db" \
+    --db_user="openeducat_db_v2_user" \
+    --database="openeducat_db_v2" \
     --db_sslmode="require" \
     -i base \
     --http-port="${PORT:-10000}"
