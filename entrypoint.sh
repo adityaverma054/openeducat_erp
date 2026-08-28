701176e25d54ba6cab27891238ac85a9d1f68b60
adityaverma054 <adityaverma054@gmail.com>
2026-08-29 02:05:40 +0530

    Add base image option to Odoo entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -e
-
 export PGPASSWORD="EWBXt5VbG57XDppAqGrrnSV8JVecaZOh"
-
 exec python3 /usr/bin/odoo \
     --db_host="dpg-da8ukrajnfac73c4mmsg-a.oregon-postgres.render.com" \
     --db_port="5432" \
     --db_user="openeducat_db_user" \
     --database="openeducat_db" \
     --db_sslmode="require" \
+    -i base \
     --http-port="${PORT:-10000}"
