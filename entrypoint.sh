26d8a59aab82974d1a70b93a6020c1452dc7e9e9
adityaverma054 <adityaverma054@gmail.com>
2026-08-29 01:51:00 +0530

    Update database connection parameters in entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
-set -e
-
-if [ -n "$DATABASE_URL" ]; then
-    DB_USER=$(echo "$DATABASE_URL" | sed -r 's#postgres(ql)?://([^:]+):([^@]+)@([^:]+):([0-9]+)/(.*)#\2#')
-    DB_PASSWORD=$(echo "$DATABASE_URL" | sed -r 's#postgres(ql)?://([^:]+):([^@]+)@([^:]+):([0-9]+)/(.*)#\3#')
-    DB_HOST=$(echo "$DATABASE_URL" | sed -r 's#postgres(ql)?://([^:]+):([^@]+)@([^:]+):([0-9]+)/(.*)#\4#')
-    DB_PORT=$(echo "$DATABASE_URL" | sed -r 's#postgres(ql)?://([^:]+):([^@]+)@([^:]+):([0-9]+)/(.*)#\5#')
-    DB_NAME=$(echo "$DATABASE_URL" | sed -r 's#postgres(ql)?://([^:]+):([^@]+)@([^:]+):([0-9]+)/(.*)#\6#')
-else
-    DB_HOST="${PGHOST:-db}"
-    DB_PORT="${PGPORT:-5432}"
-    DB_USER="${PGUSER:-odoo}"
-    DB_PASSWORD="${PGPASSWORD:-odoo}"
-    DB_NAME="${PGDATABASE:-odoo}"
-fi
-
-exec python3 /usr/bin/odoo \
-    --db_host="$DB_HOST" \
-    --db_port="$DB_PORT" \
-    --db_user="$DB_USER" \
-    --db_password="$DB_PASSWORD" \
-    --database="$DB_NAME" \
+python3 /usr/bin/odoo \
+    --db_host="dpg-cuv3s5btq21c73edp9qg-a.oregon-postgres.render.com" \
+    --db_port="5432" \
+    --db_user="openeducat_db_user" \
+    --db_password="APNA_DATABASE_PASSWORD_YAHAN_DALO" \
+    --database="openeducat_db" \
     --http-port="${PORT:-10000}"
