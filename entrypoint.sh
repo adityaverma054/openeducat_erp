07b2bb02bc93775942c0ac5fbd0ccef82e31e395
adityaverma054 <adityaverma054@gmail.com>
2026-08-29 01:53:57 +0530

    Modify database host and password in entrypoint.sh
    Updated database connection parameters for Odoo.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+export PGPASSWORD="EWBXt5VbG57XDppAqGrrnSV8JVecaZOh"
 python3 /usr/bin/odoo \
-    --db_host="dpg-cuv3s5btq21c73edp9qg-a.oregon-postgres.render.com" \
+    --db_host="dpg-da8ukrajnfac73c4mmsg-a.oregon-postgres.render.com" \
     --db_port="5432" \
     --db_user="openeducat_db_user" \
-    --db_password="APNA_DATABASE_PASSWORD_YAHAN_DALO" \
     --database="openeducat_db" \
+    --db_sslmode="require" \
     --http-port="${PORT:-10000}"
